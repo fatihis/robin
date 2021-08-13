145a4fe54c305a5710dab749c30c66df0eeba797
fatihis99@hotmail.com <isk.fatih1@gmail.com>
2021-08-13 06:31:40 +0300

    update routes

--- src/pages/shared/Shared.js
+++ src/pages/shared/Shared.js
@@ -1,35 +1,33 @@
 import React from "react";
 import Home from "../home/Home";
 import Listing from "../listing/Listing";
+import Shoppingcard from "../checkout/shoppingcard/Shoppingcard";
 import "./shared.css";
 import Productdetail from "../productdetail/Productdetail";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useRouteMatch,
-  useParams,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import logo from "../../assets/robintemp.png";
 import searchicon from "../../assets/searchicon.png";
 import carticon from "../../assets/carticon.png";
 import profileicon from "../../assets/profileicon.png";
+import home from "../home/Home";
+import Addressanddelivery from "../checkout/addressanddelivery/Addressanddelivery";
 export default function Shared() {
   return (
     <Router>
       <div className="shared-main-wrapper">
-        <div className="shared-header container bg-white h-24 flex flex-row">
+        <div className="shared-header-container bg-white h-24 flex flex-row">
           <div className="shared-logo-container flex-1 bg-green flex justify-center items-center">
             <img
-              className="shared-logo max-w-xs"
+              className="shared-logo "
               src={logo}
               alt="robinhoodie logo"
             ></img>
           </div>
           <div className="shared-nav-elements-container flex px-14 bg-gray justify-center items-center ">
             <div className="shared-nav-item flex-1 flex  items-center justify-end">
-              <p className="text-right text-xs font-bold">HOME</p>
+              <a href="/products">
+                <p className="jj text-right text-xs font-bold">HOME</p>
+              </a>
             </div>
             <div className="shared-nav-item flex-1 flex  items-center justify-center">
               <p className="text-right text-xs font-bold">PRODUCTS</p>
@@ -62,10 +60,10 @@ export default function Shared() {
             </div>
           </div>
         </div>
-        <Route path="/" component={Home} />
-        <Route path="/products" component={Listing} />
-        <Route path="/product-detail/" component={Productdetail} />
       </div>
+      <Route path="/" component={Addressanddelivery} />
+      <Route path="/products" component={Listing} />
+      <Route path="/p/" component={Productdetail} />
       <div className="shared-footer container bg-white h-48"></div>
     </Router>
   );
