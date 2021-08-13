bee22d8b9618b066ebc9e1b355313b2cfce5cba7
fatihis99@hotmail.com <isk.fatih1@gmail.com>
2021-08-13 06:31:00 +0300

    add addressanddelivery

---
+++ src/pages/checkout/addressanddelivery/Addressanddelivery.js
@@ -0,0 +1,35 @@
+import React from "react";
+import AddressForm from "../../../components/AddressForm/AddressForm";
+import DonationOption from "../../../components/DonationOption/DonationOption";
+
+export default function Addressanddelivery() {
+  return (
+    <div className="address-donation-main-wrapper  px-28 py-18  w-full h-full flex ">
+      <div className="address-donation-main-container h-full w-full  flex flex-col ">
+        <h2 className="address-donation-header-text mt-2 mb-2 font-bold text-xl">
+          Address data and type of donation
+        </h2>
+        <div className="address-donation-flex mt-10  h-full w-full flex ">
+          {/* address container start */}
+          <div className="address-container flex-1 flex  mt-4">
+            <AddressForm></AddressForm>
+          </div>
+          {/* address container end */}
+          <div className="donation-container flex-1 flex">
+            <div class="flex flex-col items-center justify-center w-full h-full">
+              <div className="flex-1">
+                <DonationOption></DonationOption>
+              </div>
+
+              <div className=" flex justify-end items-baseline">
+                <button className="shopping-cart-proceed-button absolute text-white font-bold py-2 px-4 rounded-full">
+                  Complete
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
